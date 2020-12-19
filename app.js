b2fdf6a2ac23c0290baade57c2b2b509492475ec
briantu <brianptu2@gmail.com>
2020-12-19 17:33:04 -0500

    socketio

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
 /// IMPORTS AND PRE REQS
-var express = require('express');
-var AWS = require('aws-sdk')
-var path = require('path');
-var fs = require('fs');
-var atob = require('atob')
-var im = require('imagemagick');
-var Promise = require('promise');
-var Chart = require('chart.js');
+var express = require("express");
+var AWS = require("aws-sdk");
+var path = require("path");
+var fs = require("fs");
+var atob = require("atob");
+var im = require("imagemagick");
+var Promise = require("promise");
+var Chart = require("chart.js");
+const serverSocket = require("./server-socket");
 
 // CONSTANTS AND API KEYS
 const PORT = process.env.PORT || 3000;
 
-
 const config = {
-    accessKeyId: "",
-    secretAccessKey: "",
-    region: "us-east-2"
-
+	accessKeyId: "",
+	secretAccessKey: "",
+	region: "us-east-2"
 };
 
 // Instancate OBJECTS
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // PAGE BUILDING STUFF
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
-app.get('/debug', function (req, res) {
-  res.sendFile(path.join(__dirname + '/pages/template.html'));
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get("/debug", function(req, res) {
+	res.sendFile(path.join(__dirname + "/pages/template.html"));
 });
 
-
 server.listen(PORT);
-console.log("CHECKING PORT " + PORT)
+console.log("CHECKING PORT " + PORT);
 
-
-io.on('connection', function(socket){
-
-
+io.on("connection", (socket) => {
+	serverSocket.init(io, socket);
 });
